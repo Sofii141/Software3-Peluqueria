ffb5715e1bc6ad8e3dca8b510f21024f361eb3a3
ibella2222 <isabellatrochez@unicauca.edu.co>
2025-12-02 14:38:11 -0500

    ya todo con eviroment y conectado el front al apigateway

--- src/app/estilistas/estilistas.service.ts
+++ src/app/estilistas/estilistas.service.ts
@@ -2,26 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Estilista } from './estilista.model';
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstilistasService {
 
-  private apiUsuarios = 'http://localhost:8080/api/usuarios';
-  private apiAuth = 'http://localhost:8080/api/auth';
-  private apiReservas = 'http://localhost:8081/reservas';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   /** Listar estilistas */
   listar(): Observable<Estilista[]> {
-    return this.http.get<Estilista[]>(`${this.apiUsuarios}/estilistas`);
+    return this.http.get<Estilista[]>(`${this.baseUrl}/api/usuarios/estilistas`);
   }
 
   /** Crear estilista */
   crear(estilista: Partial<Estilista>): Observable<any> {
-    return this.http.post(`${this.apiAuth}/registro`, {
+    return this.http.post(`${this.baseUrl}/api/auth/registro`, {
       nombre: estilista.nombre,
       email: estilista.email,
       telefono: estilista.telefono,
@@ -32,16 +31,16 @@ export class EstilistasService {
 
   /** Actualizar estilista */
   actualizar(id: number, estilista: Partial<Estilista>): Observable<any> {
-    return this.http.put(`${this.apiUsuarios}/${id}`, estilista);
+    return this.http.put(`${this.baseUrl}/api/usuarios/${id}`, estilista);
   }
 
   /** Inactivar estilista (solo cambia estado) */
   inactivar(id: number): Observable<any> {
-    return this.http.put(`${this.apiUsuarios}/${id}`, { estado: false });
+    return this.http.put(`${this.baseUrl}/api/usuarios/${id}`, { estado: false });
   }
 
   /** Consultar citas pendientes */
   citasPendientes(id: number): Observable<number> {
-    return this.http.get<number>(`${this.apiReservas}/estilista/${id}/pendientes`);
+    return this.http.get<number>(`${this.baseUrl}/api/reservas/estilista/${id}/pendientes`);
   }
-}  
+}
