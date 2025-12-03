543bfe93fe8c3f69c1660bb32f146d9225acef0b
ibella2222 <isabellatrochez@unicauca.edu.co>
2025-12-02 23:02:22 -0500

    ya todo con eviroment y conectado el front al apigateway

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './layout/sidebar.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'primerProyecto';
+  token: string | null = '';
+
+  ngOnInit(): void {
+    this.token = localStorage.getItem('token');
+  }
 }
